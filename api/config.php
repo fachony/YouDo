f12b589423ee8787fd83e605e5f7db9162f942c1
fachony <faris.mulaosmanovic@stu.ibu.edu.ba>
2021-03-16 22:32:46 +0100

    first

---
+++ api/config.php
@@ -0,0 +1,15 @@
+<?php
+
+class Config{
+
+const DB_HOST = "localhost";
+const DB_USERNAME = "root";
+const DB_PASSWORD = "root";
+const DB_SCHEME = "YouDo";
+
+}
+
+
+
+
+ ?>
---
+++ api/dao/UserDao.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class UserDao extends BaseDao{
+
+
+  public funtion get_user_by_email($email){
+
+
+
+  }
+}
+
+ ?>
